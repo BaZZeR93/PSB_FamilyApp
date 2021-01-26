cb22222e3d1349b13d9377792a106d87be6c3c29
Robert Roman <robertroman13@yahoo.com>
2021-01-26 22:03:36 +0200

    dashboard

--- src/app/pages/dashboard-page/dashboard-page.component.ts
+++ src/app/pages/dashboard-page/dashboard-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/user';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import { Content } from 'src/app/models/contentEnum';
 
 @Component({
   selector: 'app-dashboard-page',
@@ -6,10 +10,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard-page.component.css']
 })
 export class DashboardPageComponent implements OnInit {
+  user: any;
+  public ContentEnum = Content;
+  public selectedContent: Content = Content.Wall;
 
-  constructor() { }
+  constructor(
+    private auth: AuthenticationService,
+    private router: Router)
+  {}
 
   ngOnInit() {
+    this.user = <User>JSON.parse(localStorage.getItem('currentUser'));
   }
 
+  switchTab(tab: Content) {
+    this.selectedContent = tab;
+  }
+
+  logoutUser() {
+    this.auth.logout();
+    this.router.navigate(['/login']);
+  }
 }
